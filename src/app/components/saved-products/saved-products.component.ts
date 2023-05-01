1e8ad9a0bd7ee1f8e88d28f374bda4f8d761b7d5
iamchaze <virajkale9604@gmail.com>
2023-05-01 22:36:42 +0530

    working on auction app

--- src/app/components/saved-products/saved-products.component.ts
+++ src/app/components/saved-products/saved-products.component.ts
@@ -13,33 +13,53 @@ export class SavedProductsComponent implements OnInit {
 constructor(private service: GlobalService, private router:Router, private location:Location){}
 
   allProducts:any
-  savedProducts:any
+  savedProducts:any = []
+  currentUserId:any
+  currentUserData:any
+  savedProductsList:any
+  productObj:any
+  userObj:any
 
   ngOnInit(): void {
-    this.service.getRecords("Products").subscribe((result) =>
-      {
-        this.allProducts = result
-          // console.log(this.allProducts[0].savedProduct);
-        this.savedProducts = this.allProducts.filter((product:any) =>{
-          return product.savedProduct
+      if(sessionStorage.length > 0){
+        this.currentUserId = sessionStorage.getItem('userid')
+      } else {
+        this.currentUserId = localStorage.getItem('userid')
+      }
+      this.service.getSingleRecord("Users", this.currentUserId).subscribe((result) => {
+        this.currentUserData = result
+       this.savedProductsList = this.currentUserData.savedProductsIdList
+        this.service.getRecords("Products").subscribe((result) => {
+          this.allProducts = result
+          for(let  i= 0; i < this.savedProductsList.length; i++){
+            for(let j = 0; j < this.allProducts.length; j++){
+              if(this.allProducts[j].id == this.savedProductsList[i]){
+                this.savedProducts.push(this.allProducts[j])
+              }
+            }
+          }
         })
-        // console.log(this.savedProducts);
-
       })
   }
+
   setPath(id:any){
     this.router.navigate(['bid-products',id])
   }
 
-  unsave(id:any){
-    const tempObj = this.savedProducts.filter((product:any) => {
-      return id === product.id
-    })
-    tempObj[0].savedProduct = false
-    this.service.editRecord("Products", tempObj[0], id).subscribe((result) => {
-      alert("Product Unsaved")
-    })
 
-    this.location.go(this.location.path())
+  unsave(id:any){
+   this.service.getSingleRecord("Products",id).subscribe((result) => {
+    this.productObj = result
+    this.productObj.isSaved = false
+    this.service.editRecord("Products",this.productObj,id).subscribe((result) => {
+      alert("Product Unsaved");
+    })
+   })
+   this.service.getSingleRecord("Users", this.currentUserId).subscribe((result) => {
+    this.userObj = result
+    console.log(this.userObj);
+    this.userObj
+   })
+
   }
 }
